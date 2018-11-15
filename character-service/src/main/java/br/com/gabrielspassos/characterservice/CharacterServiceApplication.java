2d4ae7e686aaa5939fbf5765b13ba83d367947cc
Gabriel Passos <gabriel.passos@ilegra.com>
2018-11-15 12:32:33 -0200

    add character service

---
+++ character-service/src/main/java/br/com/gabrielspassos/characterservice/CharacterServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.gabrielspassos.characterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterServiceApplication.class, args);
+	}
+}
